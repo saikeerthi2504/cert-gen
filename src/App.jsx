82d5b5f911a60aaf03e2762a5164a6b58ce301e9
Saikeerthi <saikeerthinarasimha@gmail.com>
2026-05-17 15:04:32 +0530

    Modified file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-import Stepsar from "./components/Stepsbar";
+import Stepsbar from "./components/Stepsbar";
 import StepDetails from "./components/Stepdetails";
 import StepAchievement from "./components/Stepachievement";
 import StepReview from "./components/Stepreview";
